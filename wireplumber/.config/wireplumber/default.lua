547a189bfd3fdc048c86bcc9823ed9a8ec3b1130
Jack Wiegman <33030441+jackwiegman@users.noreply.github.com>
2025-02-14 22:45:56 -0700

    wireplumber update

--- wireplumber/.config/wireplumber/default.lua
+++ wireplumber/.config/wireplumber/default.lua
@@ -5,10 +5,10 @@ context.init = function()
 
 	if sink then
 		-- Set the properties to enable 7.1 upmixing
-		sink.properties["audio.channels"] = 8 -- 7.1 surround (8 channels)
-		sink.properties["audio.upmix"] = true -- Enable upmixing
+		sink.properties["audio.channels"] = 8           -- 7.1 surround (8 channels)
+		sink.properties["audio.upmix"] = true           -- Enable upmixing
 		sink.properties["audio.upmix.mode"] = "stereo-to-7.1" -- Upmix stereo to 7.1
-		sink.properties["audio.sample-rate"] = 48000 -- Sample rate (48kHz is standard for surround)
-		sink.properties["audio.format"] = "S32LE" -- Audio format (32-bit little-endian)
+		sink.properties["audio.sample-rate"] = 48000    -- Sample rate (48kHz is standard for surround)
+		sink.properties["audio.format"] = "S32LE"       -- Audio format (32-bit little-endian)
 	end
 end
