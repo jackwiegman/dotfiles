b161ec3c4165e725107ab78ea890b3ef9c65f368
Jack Wiegman <33030441+jackwiegman@users.noreply.github.com>
2025-02-18 17:41:39 -0700

    get rid of this config for now

--- wireplumber/.config/wireplumber/default.lua
+++ wireplumber/.config/wireplumber/default.lua
@@ -1,14 +1,14 @@
--- WirePlumber Policy for sink 37: upmix to 7.1 surround
-context.init = function()
-	-- Get the node (sink) by ID
-	local sink = context:find_node("34")
-
-	if sink then
-		-- Set the properties to enable 7.1 upmixing
-		sink.properties["audio.channels"] = 8 -- 7.1 surround (8 channels)
-		sink.properties["audio.upmix"] = true -- Enable upmixing
-		sink.properties["audio.upmix.mode"] = "stereo-to-7.1" -- Upmix stereo to 7.1
-		sink.properties["audio.sample-rate"] = 48000 -- Sample rate (48kHz is standard for surround)
-		sink.properties["audio.format"] = "S32LE" -- Audio format (32-bit little-endian)
-	end
-end
+-- -- WirePlumber Policy for sink 37: upmix to 7.1 surround
+-- context.init = function()
+--     -- Get the node (sink) by ID
+--     local sink = context:find_node("45")
+--
+--     if sink then
+--         -- Set the properties to enable 7.1 upmixing
+--         sink.properties["audio.channels"] = 8 -- 7.1 surround (8 channels)
+--         sink.properties["audio.upmix"] = true -- Enable upmixing
+--         sink.properties["audio.upmix.mode"] = "stereo-to-7.1" -- Upmix stereo to 7.1
+--         sink.properties["audio.sample-rate"] = 48000 -- Sample rate (48kHz is standard for surround)
+--         sink.properties["audio.format"] = "S32LE" -- Audio format (32-bit little-endian)
+--     end
+-- end
