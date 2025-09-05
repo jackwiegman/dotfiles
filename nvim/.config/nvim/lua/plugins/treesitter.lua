94b59f9eabea8f015e54829a0a182d5a5f65c946
Jack Wiegman <33030441+jackwiegman@users.noreply.github.com>
2025-09-05 16:56:55 -0600

    Nvim updates

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -14,6 +14,7 @@ return {
                 'doxygen',
                 'html',
                 'javascript',
+                'json',
                 'lua',
                 'luadoc',
                 'markdown',
@@ -22,6 +23,7 @@ return {
                 'query',
                 'vim',
                 'vimdoc',
+                'yaml',
             },
             -- Autoinstall languages that are not installed
             auto_install = true,
